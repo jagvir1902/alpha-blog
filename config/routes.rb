1b94e882d971725cc62a4e57c413063db1a43d4b
jagvir1902 <jagvir1902@gmail.com>
2016-02-28 20:01:53 +0000

    add category from UI and build integration test

--- config/routes.rb
+++ config/routes.rb
@@ -14,6 +14,8 @@ get 'login' , to: 'sessions#new'
 post 'login' , to: 'sessions#create'
 delete 'logout' , to: 'sessions#destroy'
 resources :users, except: [:new]
+
+resources :categories, except: [:destroy]
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
 
