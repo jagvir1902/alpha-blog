adeded897ba9517b3389a1fbd1501f380fa87bc4
jagvir1902 <jagvir1902@gmail.com>
2016-02-29 17:56:43 +0000

    Add categories test

---
+++ test/integration/list_categories_test.rb
@@ -0,0 +1,17 @@
+require 'test_helper'
+
+class CreateCategoriesTest < ActionDispatch::IntegrationTest
+  
+  def setup
+    @category =Category.create(name: "programming")
+    @category2 =Category.create(name: "9GAG")
+  end
+  
+  test "should shoe categories listing" do 
+    get categories_path
+    assert_template 'categories/index'
+    assert_select "a[href=?]", category_path(@category), text: @category.name
+    assert_select "a[href=?]", category_path(@category2), text: @category2.name
+  end
+  
+end
